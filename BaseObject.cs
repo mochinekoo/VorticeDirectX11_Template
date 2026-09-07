7b42d2f37e63795b2481fb2b0b1208dbaa2bb5ac
mochinekoo <mochinekoo0113@gmail.com>
2026-09-07 21:07:39 +0900

    [add] オブジェクトの基底クラスを仮追加しました

---
+++ BaseObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VorticeDirectX11_Template {
+    public abstract class BaseObject {
+
+        private readonly string name_ = "";
+
+        public int DrawHighOrder {
+            get; set;
+        } = 10000;
+        public bool IsDead {
+            get; private set;
+        }
+        public string? Tag {
+            get; set;
+        } = null;
+        public BaseObject? Parent {
+            get; set;
+        } = null;
+        public List<BaseObject> ChildList {
+            get; set;
+        } = new List<BaseObject>();
+        public Transform Transform {
+            get; set;
+        } = new Transform();
+
+        public BaseObject(string name) {
+            name_ = name;
+        }
+
+        public abstract void Init();
+        public abstract void Update();
+        public abstract void Draw();
+        public virtual void OnCollision() { }
+
+    }
+}
---
+++ Transform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using Vortice.Mathematics;
+
+namespace VorticeDirectX11_Template {
+    public class Transform {
+
+        public Double3 location_;
+        public Double3 velocity_;
+        public Double3 rotation_;
+        public Double3 scale_;
+
+        public Transform() {
+            location_ = new Double3(0, 0, 0);
+            velocity_ = new Double3(0, 0, 0);
+            rotation_ = new Double3(0, 0, 0);
+            scale_ = new Double3(1, 1, 1);
+        }
+
+        Matrix4x4 GetWorldMatrix() {
+            Matrix4x4 scale = Matrix4x4.CreateScale((float) scale_.X, (float)scale_.Y, (float)scale_.Z);
+            Matrix4x4 rotation = Matrix4x4.CreateFromYawPitchRoll((float)rotation_.Y, (float)rotation_.X, (float)rotation_.Z);
+            Matrix4x4 translation = Matrix4x4.CreateTranslation((float)location_.X, (float)location_.Y, (float)location_.Z);
+            return scale * rotation * translation;
+
+        }
+
+    }
+}
