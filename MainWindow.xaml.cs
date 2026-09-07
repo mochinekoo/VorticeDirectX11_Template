7d6348d701593b643638e5b4d54e0c84699983c7
mochinekoo <mochinekoo0113@gmail.com>
2026-09-07 20:08:36 +0900

    [add]  https://github.com/mochinekoo/VorticeDirectX_Sample より作成

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vortice.Wpf;
 
 namespace VorticeDirectX11_Template
 {
@@ -16,9 +17,30 @@ namespace VorticeDirectX11_Template
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Render DrawRender { get; private set; } = new Render();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ViewSurface.LoadContent += LoadViewSurface;
+            ViewSurface.Draw += DrawViewSurface;
+            ViewSurface.UnloadContent += UnLoadViewSurface;
+        }
+
+        public void LoadViewSurface(object? sender, DrawingSurfaceEventArgs e)
+        {
+            DrawRender.Init(e);
+        }
+
+        public void DrawViewSurface(object? sender, DrawEventArgs e)
+        {
+            DrawRender.Draw(e);
+        }
+
+        public void UnLoadViewSurface(object? sender, DrawingSurfaceEventArgs e)
+        {
+            DrawRender.Release(e);
         }
     }
 }
